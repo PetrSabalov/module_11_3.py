2f187934097a89a2137293cb1fd0e07fd23a1366
PetrSabalov <petrsabalov@gmail.com>
2025-01-10 19:16:33 +0300

    module_11_3.py

--- module_11_3.py
+++ module_11_3.py
@@ -1,7 +1,13 @@
 def introspection_info(obj):
     obj_type = type(obj).__name__
-    attributes = dir(obj)
-    methods = [method for method in attributes if callable(getattr(obj, method))]
+    attributes = []
+    methods = []
+    for attr in dir(obj):
+        if callable(getattr(obj, attr)):
+            methods.append(attr)
+    else:
+        attributes.append(attr)
+
     module = obj.__class__.__module__
     info = {'type': obj_type, 'attributes': attributes, 'methods': methods, 'module': module}
     return info
